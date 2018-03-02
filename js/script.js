af32bd6a6523cc812313a8c35e934cb09be5c9d7
Karl Essinger <xkaess22@gmail.com>
2018-03-02 12:41:04 +0100

    Fack yu

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function openEquipmentMenu(id) {
 
     if (currentBoi != id) {
         document.getElementById(id).style.display = "block";
-        document.getElementById("itemList").style["overflow-y"] = "scroll";
+        document.getElementById("itemList").style["overflow-y"] = "auto";
         currentBoi = id;
     }
     else {
@@ -24,16 +24,14 @@ function clearEquipmentMenu() {
     currentBoi = "";
 }
 
-function loadCharacter(id)
-{
-  if (id == "ash") {
-    document.getElementById("loade").innerHTML='<object type="text/html" data="ash.html" ></object>';
-  } else if (id == "fuze") {
-    document.getElementById("loade").innerHTML='<object type="text/html" data="Fuze.html" ></object>';
-  } else if (id == "rook") {
-    document.getElementById("loade").innerHTML='<object type="text/html" data="Rook.html" ></object>';
-  } else if (id == "jeger") {
-    document.getElementById("loade").innerHTML='<object type="text/html" data="Jeger.html" ></object>';
-  }
-
+function loadCharacter(id) {
+    if (id == "ash") {
+        document.getElementById("loade").innerHTML = '<object type="text/html" data="ash.html" ></object>';
+    } else if (id == "fuze") {
+        document.getElementById("loade").innerHTML = '<object type="text/html" data="Fuze.html" ></object>';
+    } else if (id == "rook") {
+        document.getElementById("loade").innerHTML = '<object type="text/html" data="Rook.html" ></object>';
+    } else if (id == "jeger") {
+        document.getElementById("loade").innerHTML = '<object type="text/html" data="Jeger.html" ></object>';
+    }
 }
