d6ff3c33f7a02847b7bd166d6f49773f14ec66e2
Karl Essinger <xkaess22@gmail.com>
2018-03-02 10:33:03 +0100

    Auto-cleanup

--- js/script.js
+++ js/script.js
@@ -1,36 +1,29 @@
 // JavaScript source code
 var currentBoi = "";
 
-function openEquipmentMenu(id)
-{
-    if (currentBoi != "")
-    {
+function openEquipmentMenu(id) {
+    if (currentBoi != "") {
         document.getElementById(currentBoi).style.display = "none";
         document.getElementById("itemList").style["overflow-y"] = "hidden";
     }
 
-    if (currentBoi != id)
-    {
+    if (currentBoi != id) {
         document.getElementById(id).style.display = "block";
         document.getElementById("itemList").style["overflow-y"] = "scroll";
         currentBoi = id;
     }
-    else
-    {
+    else {
         currentBoi = "";
     }
 }
-function clearEquipmentMenu()
-{
-    if (currentBoi != "")
-    {
+function clearEquipmentMenu() {
+    if (currentBoi != "") {
         document.getElementById(currentBoi).style.display = "none";
     }
     document.getElementById("itemList").style["overflow-y"] = "hidden";
     currentBoi = "";
 }
 
-function loadCharacter()
-{
-    document.getElementById("loade").innerHTML='<object type="text/html" data="ash.html" ></object>';
+function loadCharacter() {
+    document.getElementById("loade").innerHTML = '<object type="text/html" data="ash.html" ></object>';
 }
